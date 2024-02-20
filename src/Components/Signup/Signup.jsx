c69ddd8318492eddc914334c1fb4ca35db6478c7
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-02-20 15:09:05 +0500

    cards made successfully

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState } from "react";
+
 import { Link, useNavigate } from "react-router-dom";
 
 import {
@@ -14,19 +16,28 @@ import {
   CardFooter,
 } from "@chakra-ui/react";
 
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, StarIcon, CheckIcon } from "@chakra-ui/icons";
 
-// import "./Login.css";
+import "./Signup.css";
 import CustomInputs from "../../Custom/CustomInputs";
 
 function Signup() {
+  const [expanded, setExpanded] = useState(false);
   const navigate = useNavigate();
 
-  const commonFlexProperties = {
-    width: "1000px",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
+  const minidivCommomProperties = {
+    display: "inline-block",
+    width: "90px",
+    height: "90px",
+    margin: "10px",
+    // padding: "5px",
+    borderRadius: "30px",
+    border: "1px solid",
+    borderColor: "rgb(176, 67, 67)",
+  };
+
+  const handleExpand = () => {
+    setExpanded(true);
   };
 
   const handleSignupSubmitBtn = () => {
@@ -39,9 +50,11 @@ function Signup() {
     <Box bgColor="black">
       <Flex justifyContent="space-around">
         <Flex
-          sx={commonFlexProperties}
+          width="1000px"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
           direction="column"
-          backgroundImage="url('src\assets\Pictures\pexels-ketut-subiyanto-5037345.jpg')"
           backgroundSize="cover"
           className="backgroundpicdiv"
         >
@@ -64,9 +77,22 @@ function Signup() {
           </Heading>
         </Flex>
 
-        <Flex sx={commonFlexProperties}>
+        <Flex
+          cursor="pointer"
+          width="1000px"
+          justifyContent="center"
+          height="100vh"
+          overflowY="auto"
+          sx={{
+            "::webkit-scrollbar": {
+              display: "none",
+            },
+          }}
+        >
           <Stack spacing={3}>
-            <Heading color="white">Sign Up</Heading>
+            <Heading color="white" mt="20px">
+              Sign Up
+            </Heading>
             <Text color="rgb(141 157 188)">
               Already have an account{" "}
               <Link to="/login">
@@ -80,27 +106,322 @@ function Signup() {
               </Link>
             </Text>
 
-            {/* start from here and make the cards */}
             <Text color="#ff7565">Step 1</Text>
             <Heading size="lg" color="white">
               Select Your Plan
             </Heading>
 
-            <Box></Box>
             <Card
               bgColor="black"
               border="3px solid"
               borderColor="#ff7565"
-              borderRadius="10px"
+              borderRadius="30px"
               width="400px"
+              margin="0px"
+              padding="0px"
+              onClick={handleExpand}
             >
               <CardBody>
-                <Heading size="md" color="white">
-                  Annual
+                <Flex direction="row" justifyItems="baseline">
+                  <HamburgerIcon
+                    bgColor="#ff7565"
+                    color="white"
+                    m="5px"
+                    width="20px"
+                    height="18px"
+                  />
+                  <Heading size="md" color="white">
+                    Annual
+                  </Heading>
+                </Flex>
+
+                <Heading size="small" color="#8d9dbc" mt="5px">
+                  $9.99 USD/PER MONTH • BILLED ANNUALLY
                 </Heading>
-                <Text>
-                  View a summary of all your customers over the last month.
+                <Text color="#8d9dbc" size="small" mt="10px">
+                  At the end of your 30 day trial your subscription will
+                  automatically rollover to a $119.99/year subscription billed
+                  annually unless cancelled.
                 </Text>
+
+                <Flex
+                  direction="row"
+                  justifyItems="baseline"
+                  mt="10px"
+                  mb="20px"
+                >
+                  <StarIcon
+                    bgColor="#ff7565"
+                    color="white"
+                    m="5px"
+                    width="20px"
+                    height="18px"
+                  />
+                  <Heading size="small" color="white">
+                    RECOMMENDED
+                  </Heading>
+                </Flex>
+
+                {expanded && (
+                  <CardBody>
+                    <Heading size="small" color="white" mt="15px">
+                      All programmes Included
+                    </Heading>
+
+                    <Box
+                      overflowY="auto"
+                      whiteSpace="nowrap"
+                      sx={{
+                        "::-webkit-scrollbar": {
+                          display: "none",
+                        },
+                      }}
+                    >
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      >
+                        {/* <Text>Lorem ipsum dolor sit</Text> */}
+                      </Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                      <Box
+                        className="minidivs"
+                        sx={minidivCommomProperties}
+                        // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                      ></Box>
+                    </Box>
+
+                    <Flex direction="row" color="white">
+                      <CheckIcon
+                        m="4px"
+                        bgColor="#ff7565"
+                        borderRadius="100px"
+                      />
+                      <Text fontWeight="bold">100+ Workouts For Any Goal</Text>
+                    </Flex>
+                    <Flex direction="row" color="white">
+                      <CheckIcon
+                        m="4px"
+                        bgColor="#ff7565"
+                        borderRadius="100px"
+                      />
+                      <Text fontWeight="bold">
+                        Choose Over 10 Million Branded Foods
+                      </Text>
+                    </Flex>
+                    <Flex direction="row" color="white">
+                      <CheckIcon
+                        m="4px"
+                        bgColor="#ff7565"
+                        borderRadius="100px"
+                      />
+                      <Text fontWeight="bold">
+                        Wide Variety Of Delicious Recipes
+                      </Text>
+                    </Flex>
+                    <Flex direction="row" color="white">
+                      <CheckIcon
+                        m="4px"
+                        bgColor="#ff7565"
+                        borderRadius="100px"
+                      />
+                      <Text fontWeight="bold">
+                        Access To Our Exclusive Community
+                      </Text>
+                    </Flex>
+                  </CardBody>
+                )}
+              </CardBody>
+            </Card>
+
+            <Card
+              bgColor="black"
+              border="3px solid"
+              borderColor="#ff7565"
+              borderRadius="30px"
+              width="400px"
+              //   pointer="cursor"
+            >
+              <CardBody>
+                <Flex direction="row" justifyItems="baseline">
+                  <HamburgerIcon
+                    bgColor="#ff7565"
+                    color="white"
+                    m="5px"
+                    width="20px"
+                    height="18px"
+                  />
+                  <Heading size="md" color="white">
+                    Monthly
+                  </Heading>
+                </Flex>
+
+                <Heading size="small" color="#8d9dbc" mt="5px">
+                  $19.99 USD/PER MONTH • BILLED ANNUALLY
+                </Heading>
+                <Text color="#8d9dbc" size="small" mt="10px">
+                  At the end of your 30 day trial your subscription will
+                  automatically rollover to a $119.99/year subscription billed
+                  annually unless cancelled.
+                </Text>
+
+                <Flex
+                  direction="row"
+                  justifyItems="baseline"
+                  mt="10px"
+                  mb="20px"
+                >
+                  <StarIcon
+                    bgColor="#ff7565"
+                    color="white"
+                    m="5px"
+                    width="20px"
+                    height="18px"
+                  />
+                  <Heading size="small" color="white">
+                    RECOMMENDED
+                  </Heading>
+                </Flex>
+
+                <Heading size="small" color="white" mt="15px">
+                  All programmes Included
+                </Heading>
+
+                <Box
+                  overflowY="auto"
+                  whiteSpace="nowrap"
+                  sx={{
+                    "::-webkit-scrollbar": {
+                      display: "none",
+                    },
+                  }}
+                >
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  >
+                    {/* <Text>Lorem ipsum dolor sit</Text> */}
+                  </Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                  <Box
+                    className="minidivs"
+                    sx={minidivCommomProperties}
+                    // backgroundImage="url('E:\React\chakraUI\src\assets\Pictures\Login-page-imagr.jpg')"
+                  ></Box>
+                </Box>
+
+                <Flex direction="row" color="white">
+                  <CheckIcon m="4px" bgColor="#ff7565" borderRadius="100px" />
+                  <Text fontWeight="bold">100+ Workouts For Any Goal</Text>
+                </Flex>
+                <Flex direction="row" color="white">
+                  <CheckIcon m="4px" bgColor="#ff7565" borderRadius="100px" />
+                  <Text fontWeight="bold">
+                    Choose Over 10 Million Branded Foods
+                  </Text>
+                </Flex>
+                <Flex direction="row" color="white">
+                  <CheckIcon m="4px" bgColor="#ff7565" borderRadius="100px" />
+                  <Text fontWeight="bold">
+                    Wide Variety Of Delicious Recipes
+                  </Text>
+                </Flex>
+                <Flex direction="row" color="white">
+                  <CheckIcon m="4px" bgColor="#ff7565" borderRadius="100px" />
+                  <Text fontWeight="bold">
+                    Access To Our Exclusive Community
+                  </Text>
+                </Flex>
               </CardBody>
             </Card>
 
